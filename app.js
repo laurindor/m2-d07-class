dbbbb706f44e3972328f74b19d092f60dc61bcb7
laurindor <arual.socram@gmail.com>
2021-06-30 15:05:53 +0200

    changes

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ const app = express();
 // require database configuration
 require('./configs/db.config');
 
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
@@ -24,9 +28,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+
+// SESSION MIDDLEWARE
+// Checks incoming request: if there is a cookie, and if cookie has valid session id
+app.use(
+  session({
+    secret: "bananarama",
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 7 * 24 * 60 * 60, // Default value - 14 days
+    }),
+  })
+);
+
 // ROUTES
 app.use("/auth", authRouter);
-
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler

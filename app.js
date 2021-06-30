64ac9acf4088ed960a6fced913a295bf55cd66f4
laurindor <arual.socram@gmail.com>
2021-06-30 15:19:08 +0200

    fixing problems

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ require('./configs/db.config');
 
 
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -33,13 +33,15 @@ app.use(express.static(path.join(__dirname, "public")));
 // Checks incoming request: if there is a cookie, and if cookie has valid session id
 app.use(
   session({
-    secret: "bananarama",
+    secret: 'PizzaBytes',
     resave: true,
-    saveUninitialized: false,
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-      ttl: 7 * 24 * 60 * 60, // Default value - 14 days
-    }),
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000
+    },
+    store: MongoStore.create({
+      mongoUrl: 'mongodb://localhost/auth-demo'
+    })
   })
 );
 

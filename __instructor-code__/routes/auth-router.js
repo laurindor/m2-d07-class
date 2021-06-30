570643f0ff22b4593fb6f646f9d41d4d6ad66871
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-30 10:55:19 +0200

    fixed auth-route for students

--- __instructor-code__/routes/auth-router.js
+++ __instructor-code__/routes/auth-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authRouter = express.Router();
-const User = require("./../models/user-model");
+const User = require("../models/user-model");
 
 const bcrypt = require("bcrypt");
 const saltRounds = 5;
